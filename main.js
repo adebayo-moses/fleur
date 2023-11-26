f84ab02268434c11e8e86238085e7ddceffc6ca2
adebayo-moses <adebayomoses597@gmail.com>
2023-11-26 12:13:57 +0100

    functions, tailwind and ui update

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import { Pane } from "tweakpane";
 
 // function toggleTheme() {
 //   if (document.body.classList.contains("dark")) document.body.classList.remove("dark");
@@ -8,3 +9,25 @@ window.toggleTheme = function () {
   if (document.body.classList.contains("dark")) document.body.classList.remove("dark");
   else document.body.classList.add("dark");
 };
+
+// check if debug param is present or environment is dev
+const hasDebugParam = new URLSearchParams(window.location.search).has("debug");
+const isDev = process.env.NODE_ENV === "development";
+
+// create settings pane if debug enabled
+let pane;
+if (hasDebugParam || isDev) {
+  // create pane component
+  pane = new Pane({
+    title: "Settings",
+    expanded: isDev, // start expanded in dev env
+  });
+
+  // mount to DOM
+  const root = document.getElementById("main");
+  root.appendChild(pane.render());
+}
+
+// interact with pane using pane instance
+pane.toggleExpansion();
+pane.update({ title: "New Title" });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const config = {
   darkMode: "class", // This enables dark mode
   theme: {
     extend: {},
+    darkMode: "class",
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {},
